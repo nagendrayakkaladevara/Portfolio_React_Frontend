5a9d7fa1b5977fe0d618aae141560d05bd928339
nagendrayakkaladevara <nagendrayakkaladevara@gmail.com>
2024-06-02 20:04:08 +0530

    added commend prompt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/footer';
+import NavBar from './components/navbar';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Application from './pages/applocation';
+import About from './pages/about/about';
+import Contacts from './pages/contacts/contacts';
+import Projects from './pages/projects/projects';
+import Blog from './pages/blog/blog';
+import ArticlesAndresourcess from './pages/articlesAndresources/articlesAndresourcess';
+import Brandingvalues from './pages/brandingvalues/brandingvalues';
+import ResponsiveApplications from './pages/responsiveApplications/responsiveApplications';
+import Psdtohtml from './pages/psdtohtml/psdtohtml';
+import Project from './pages/projects/project';
+import BlogPost from './pages/blog/blogPost';
+import { useEffect } from 'react';
+import SmoothScroll from './components/SmoothScroll';
 
 function App() {
+  useEffect(() => {
+    window.scrollTo(0, 0); // Scrolls to the top when component mounts
+  }, []); 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <SmoothScroll />
+        <div className='main'>
+          <NavBar />
+          <Routes>
+            <Route
+              path="/"
+              element={<Application />}
+            ></Route>
+            <Route
+              path="/about"
+              element={<About />}
+            ></Route>
+            <Route
+              path="/contacts"
+              element={<Contacts />}
+            ></Route>
+            <Route
+              path="/projects"
+              element={<Projects />}
+            ></Route>
+            <Route
+              path="/blog"
+              element={<Blog />}
+            ></Route>
+            <Route path='/articlesAndresources' element={<ArticlesAndresourcess />} />
+            <Route path="/brandingvalues" element={<Brandingvalues />} />
+            <Route path="/responsiveApplications" element={<ResponsiveApplications />} />
+            <Route path="/psdtohtml" element={<Psdtohtml />} />
+            <Route path="/project/:projectName" element={<Project />} />
+            <Route path="/blog/:blogId" element={<BlogPost />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+    </>
   );
 }
 
