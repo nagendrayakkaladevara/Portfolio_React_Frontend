a7c4a23db5a65f22f2f0a363ef2efe969c445983
nagendrayakkaladevara <nagendrayakkaladevara@gmail.com>
2024-09-03 13:25:57 +0530

    added custom mouse curser

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -223,7 +223,7 @@ const About = () => {
                             <p>14/Mar/2022 - Till date </p>
                             <p>Responsibility : Web Developer</p>
                             <p>Tech Used : React,Java,Mysql,Microsoft Sharepoint.</p>
-                            <p>No of Applications Developed: 04</p>
+                            <p>No of Applications Developed: 05</p>
                         </div>
                     </div>
                 </section>
@@ -245,7 +245,7 @@ const About = () => {
                 </section>
             </div>
             <section id="section-Technologies">
-                <p className='TechnologiesHeading'>Technologies</p>
+                <p className='TechnologiesHeading flex justify-center'>Technologies</p>
                 <div className='flex gap-5 flex-wrap justify-evenly' style={{ padding: "0px 10px" }}>
                     {data.map((item, index) => (
                         <div key={index} className='flex gap-3 items-center techblur p-5'>
@@ -255,7 +255,7 @@ const About = () => {
                     ))}
                 </div>
             </section>
-            <p className='TechnologiesHeading'>IDE&apos;s</p>
+            <p className='TechnologiesHeading flex justify-center'>IDE&apos;s</p>
             <div className='flex gap-5 flex-wrap justify-evenly'>
                 {IDEData.map((item, index) => (
                     <div key={index} className='flex gap-3 items-center techblur p-5'>
@@ -264,7 +264,7 @@ const About = () => {
                     </div>
                 ))}
             </div>
-            <p className='TechnologiesHeading'>Version controls</p>
+            <p className='TechnologiesHeading flex justify-center'>Version controls</p>
             <div className='flex gap-5 flex-wrap justify-evenly'>
                 {VersionControls.map((item, index) => (
                     <div key={index} className='flex gap-3 items-center techblur p-5'>
@@ -274,7 +274,7 @@ const About = () => {
                 ))}
             </div>
             <section id="section-Hobbies">
-                <p className='TechnologiesHeading'>Hobbies</p>
+                <p className='TechnologiesHeading flex justify-center'>Hobbies</p>
                 <div className='flex justify-center'>
                     <table style={{ width: "80%" }}>
                         {HobbiesData.map(hobby => (
@@ -294,7 +294,7 @@ const About = () => {
                 </div>
             </section>
 
-            <p className='TechnologiesHeading'>My Suggestions <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-emoji-smile" viewBox="0 0 16 16" style={{ display: "inline", marginTop: "-5px", marginLeft: "5px" }}>
+            <p className='TechnologiesHeading flex justify-center items-end'>My Suggestions <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-emoji-smile" viewBox="0 0 16 16" style={{ display: "inline", marginTop: "-5px", marginLeft: "5px" }}>
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
                 <path d="M4.285 9.567a.5.5 0 0 1 .683.183A3.5 3.5 0 0 0 8 11.5a3.5 3.5 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5m4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5" />
             </svg></p>
